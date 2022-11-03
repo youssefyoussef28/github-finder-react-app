681442a59516579a143bc2e2f2f5597c7e95dfbf
Youssef Youssef <youssefyoussef28@gmail.com>
2022-11-03 19:38:08 +0200

    Applying the useContext to fetch and display

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,27 +2,15 @@ import React from "react";
 import { useEffect, useState } from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
+import GithubContext from "../../context/github/GithubContext";
+import { useContext } from "react";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { fetchUsers, users, loading } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-    const data = await response.json();
-
-    console.log(data);
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
