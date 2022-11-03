703fc9a61eecc9049eff132a983fa38073610868
Youssef Youssef <youssefyoussef28@gmail.com>
2022-11-03 13:38:59 +0200

    404 page and Routes Done

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1 className="text-6xl mb-4">Github Finder</h1>
+      <h2 className="text-2xl mb-4">
+        This WebApp is part of a paid course by Traversy Media "React Front to
+        back 2022" on Udemy .
+      </h2>
+      <h2 className="text-2xl mb-4">Completed by Youssef Youssef.</h2>
+    </div>
+  );
+}
+
+export default About;
